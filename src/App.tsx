70d8f916f78aa07981b2b74eabdd65a2832d33f5
MrlTravelGit <mickfelipe.mb@gmail.com>
2025-12-30 08:29:48 -0300

    v1

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ color: "white", padding: 40 }}>
-      <h1>MRL Landing funcionando 🚀</h1>
-      <p>Se você está vendo isso, o React está OK.</p>
-    </div>
-  );
-}
